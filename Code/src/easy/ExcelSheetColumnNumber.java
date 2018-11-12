301db9611d1678138d0ba6995e4464311cfe99ea
1Shaun <peitiantian@bupt.edu.cn>
2018-11-12 12:36:36 +0800

    171. Excel表列序号

---
+++ Code/src/easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class ExcelSheetColumnNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "AB";
+		int k = titleToNumber(s);
+		System.out.println(k);
+	}
+	public static int titleToNumber(String s) {
+		if(s.length() == 0) return 0;
+		int length = s.length(),sum = 0,k = length-1;
+		char[] cs = s.toCharArray();
+		for(int i = 0; i < length; i++) {	
+			sum +=(int) (Math.pow(26, k) * (cs[i] - 64));
+			System.out.println(sum);
+			k--;
+		}
+		return sum;
+	}
+
+}
