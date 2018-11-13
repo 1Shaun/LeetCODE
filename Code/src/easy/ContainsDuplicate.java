e7044f6514a43eeaf5a6a8f76ee56248000e046d
1Shaun <peitiantian@bupt.edu.cn>
2018-11-13 15:46:23 +0800

    no message

--- Code/src/easy/ContainsDuplicate.java
+++ Code/src/easy/ContainsDuplicate.java
@@ -11,11 +11,12 @@ public class ContainsDuplicate {
 	}
 	
 	public static boolean containsDuplicate(int[] nums) {
+		if(nums.length == 0 || nums.length == 1) return false;
 		Arrays.sort(nums);
 		for(int i = 1; i < nums.length; ++i) {
-			if(nums[i -1] == nums[i]) return false;
+			if(nums[i - 1] == nums[i]) return true;
 		}
-		return true;
+		return false;
 	}
 
 }
