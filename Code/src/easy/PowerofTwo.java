1bf1f3ad2cebd69649fd4bae62d01be972033efd
1Shaun <peitiantian@bupt.edu.cn>
2018-12-06 12:15:42 +0800

    231. 2的幂

---
+++ Code/src/easy/PowerofTwo.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class PowerofTwo {
+
+	public static void main(String[] args) {
+		int n = 2;
+		boolean flag = isPowerOfTwo(n);
+		System.out.println(flag);
+	}
+    public static boolean isPowerOfTwo(int n) {
+    	if(n == 1) return true;
+    	double f = n;
+    	while(f >= 1) {
+    		f = f / 2;
+    		
+    		if(f == 1) {
+    			return true;
+    		}
+    	}
+        return false;
+    }
+
+}
